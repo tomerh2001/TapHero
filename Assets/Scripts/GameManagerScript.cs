94678c3142f48c5f03c3d208764df493e1a06372
Tomergt45 <tomer@yaelsoft.com>
2019-05-02 00:26:31 +0300

    First commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    private static string SphereObjectPath = "Sphere";
+    private static GameObject SphereObject;
+    GameObject[] Spheres = GameObject.FindGameObjectsWithTag("Sphere");
+
+    public static GameObject AddSphere()
+    {
+        return Instantiate(SphereObject);
+    }
+
+    void Start()
+    {
+        //object obj = Resources.Load("Sphere");
+        //print(obj);
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var mouseLoc = Input.mousePosition;
+            var sphereLoc = Spheres[0].transform.position;
+            if(mouseLoc.x == sphereLoc.x && mouseLoc.y == sphereLoc.y)
+            {
+                print("CLICK DETECTED!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SphereHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereHandler : MonoBehaviour
+{
+    public static int SphereCount = 1;
+    public float CreationTime { get; set; }
+
+    void Start()
+    {
+        CreationTime = Time.time;
+    }
+
+    void Update()
+    {
+        var t = Time.time;
+
+        if (t - this.CreationTime >= 2)
+        {
+            //Destroy(this.gameObject);
+            //GameManagerScript.AddSphere();
+        }
+    }
+}
